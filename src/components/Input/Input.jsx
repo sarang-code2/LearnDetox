f34b25b6cfd63ddf2adf9c278c7e2b0725aa432a
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-05-12 17:55:49 +0600

    [feat] Input validation

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import {TextInput} from 'react-native';
 import {styles} from './InputStyle';
+import {useController} from 'react-hook-form';
 
-const Input = ({...props}) => {
-  return <TextInput style={styles.input} {...props} />;
+const Input = ({name, control}) => {
+  const {field} = useController({
+    control,
+    defaultValue: '',
+    name,
+  });
+  return (
+    <TextInput
+      value={field.value}
+      onChangeText={field.onChange}
+      style={styles.input}
+    />
+  );
 };
 
 export default Input;
--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -5,37 +5,54 @@ import {useSignInMutation} from '../../store/services/signInApi';
 import {useDispatch, useSelector} from 'react-redux';
 import {setAuth} from '../../store/slices/authSlice';
 
+import {useForm} from 'react-hook-form';
+
 const Login = ({navigation}) => {
   const dispatch = useDispatch();
+
+  const {
+    control,
+    formState: {errors},
+    handleSubmit,
+  } = useForm();
+
   const [email, onChangeEmail] = useState('');
   const [password, onChangePassword] = useState('');
 
-  const [signIn, {data, isLoading, isSuccess}] = useSignInMutation();
+  const [signIn, {isLoading, isSuccess}] = useSignInMutation();
   const {token, auth} = useSelector(state => state.auth);
 
   // console.log('auth is', auth);
 
-  const handleLogin = async () => {
-    await signIn({email, password});
+  const onSubmit = async data => {
+    // await signIn({email, password});
+    console.log('email is', data.email);
+    console.log('password is', data.password);
   };
 
   // setToken to redux store
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(setAuth(data?.response?.records));
-      navigation.navigate('Profile');
-    }
-  }, [isSuccess, data?.response?.records, dispatch, navigation]);
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     dispatch(setAuth(data?.response?.records));
+  //     navigation.navigate('Profile');
+  //   }
+  // }, [isSuccess, data?.response?.records, dispatch, navigation]);
+  // }, [isSuccess, data?.response?.records, dispatch, navigation]);
 
   return (
     <View>
       <Input
-        onChangeText={onChangeEmail}
+        name="email"
+        control={control}
+        rules={{required: true}}
         placeholder="e.g, johnson@gmail.com"
       />
-      <Input onChangeText={onChangePassword} placeholder="**********" />
+      {errors.email?.type === 'required' && <Text>First name is required</Text>}
+      <Text>First name is required</Text>
+      <Input name="password" control={control} placeholder="**********" />
       <Button
-        onPress={handleLogin}
+        // onPress={handleLogin}
+        onPress={handleSubmit(onSubmit)}
         style={{backgroundColor: 'green', padding: 10}}>
         <Text>Login</Text>
       </Button>
