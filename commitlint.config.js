c229928fd60277d84d9ea9532763ce84f98efad8
Apel Mahmud <apelmahmud.dev@gmail.com>
2024-09-26 00:23:14 +0600

    commitlint.config.js

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+// commitlint.config.js
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'revert'],
+    ],
+    'subject-case': [0], // allow any case for the subject
+  },
+};
